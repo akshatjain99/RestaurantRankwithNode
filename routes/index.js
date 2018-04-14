920336f3b9bd2e09a9ba54266b8413d8012e0c06
Akshat Jain <jain.akshat546@gmail.com>
2018-04-14 20:54:13 +0530

    Starter Files

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  const aj={name:'akshat', age:19, cool:true};
+  //res.send(req.query.name);
+  res.render('hello');
+});
+
+router.get('/reverse/:name', (req,res)=>{
+	const reverse=[...req.params.name].reverse().join('');
+	res.send(reverse);
+});
+
