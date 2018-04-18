2c6d8745d1f999f16cc59050be9e5ae8422a79d3
Akshat Jain <jain.akshat546@gmail.com>
2018-04-18 19:00:30 +0530

    Created editStore

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -4,7 +4,6 @@ const Store=mongoose.model('Store'); //importing the mdoel from store.js
 
 exports.homePage=(req,res)=>{
   res.render('index');
-  console.log(req.name);
 };
 
 
@@ -15,7 +14,27 @@ exports.addStore=(req,res)=>{
 exports.createStore=async (req,res)=>{
   //res.json(req.body); //req.body contains all the form information from the body
   const store=new Store(req.body);
-  await store.save();
+  await store.save(); //mongoose returns a promise so we need to await that promise to come back
+  req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
   res.redirect('/');
 
 };
+
+exports.getStores=async (req,res)=>{
+  const stores=await Store.find(); //Query all the stores
+  res.render('stores',{title:'Stores', stores:stores});
+};
+
+exports.editStore=async (req,res)=>{
+  const store=await Store.findOne({_id:req.params.id});
+  res.render('editStore', {title:`Edit ${store.name}`, store:store});
+}
+
+exports.updateStore=async (req,res)=>{
+  const store=await Store.findOneAndUpdate({_id:req.params.id}, req.body, {
+    new:true, //return the new store instead of the old one
+    runValidators:true
+  }).exec();
+  req.flash('success', `Successfully updated ${store.name}! <a href="/store/${store.name}"> View Store</a>`);
+  res.redirect(`/stores/${store.id}/edit`);
+}
