e20642a1e192c52711b166edc3318f1d02da4880
Akshat Jain <jain.akshat546@gmail.com>
2018-04-18 22:39:27 +0530

    Minor bug fixes

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -31,6 +31,9 @@ exports.editStore=async (req,res)=>{
 }
 
 exports.updateStore=async (req,res)=>{
+  //to set the location type as point to display on map
+  req.body.location.type="Point";
+
   const store=await Store.findOneAndUpdate({_id:req.params.id}, req.body, {
     new:true, //return the new store instead of the old one
     runValidators:true
