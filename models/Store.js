ebf10267ad503f0b373825969bfd99db1600a3e8
Akshat Jain <jain.akshat546@gmail.com>
2018-04-19 20:47:37 +0530

    Fixed multiple slugs bug

--- models/Store.js
+++ models/Store.js
@@ -35,13 +35,19 @@ const storeSchema=new mongoose.Schema({
   }
 });
 
-storeSchema.pre('save', function(next){
+storeSchema.pre('save', async function(next){
   if(!this.isModified('name')){ //Only run the function when the name of the store has been modified and not after every save
     next();
     return;
   };
 
   this.slug=slug(this.name);
+  //Find other stores of same slug
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+  if(storesWithSlug.length) {
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+  }
   next();
 });
 
